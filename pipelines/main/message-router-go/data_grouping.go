2fad44e313bfad19be41395bdd2878aac62d1002
kaichao <wukaichao@gmail.com>
2023-12-06 18:44:49 +0800

    add fit-merger to pipeline main(+retry).

--- pipelines/main/message-router-go/data_grouping.go
+++ pipelines/main/message-router-go/data_grouping.go
@@ -36,6 +36,7 @@ func parseDataSet(t string) *DataSet {
 }
 
 func initDataGrouping(dataset *DataSet) {
+
 	fmtDatDataSet := ` {
 		"datasetID":"dat:%s",
 		"keyGroupRegex":"^([0-9]+)/[0-9]+_([0-9]+)_ch([0-9]{3}).dat$",
@@ -45,14 +46,27 @@ func initDataGrouping(dataset *DataSet) {
 		"groupType":"V",
 		"verticalStart": %d,
 		"verticalHeight": %d,
-		"groupSize": %d,
-		"interleaved":true
+		"groupSize": %d
 	}
 	`
 	numPerGroup := 10
-	// 删除空格
+	// Remove space characters
 	format := regexp.MustCompile("\\s+").ReplaceAllString(fmtDatDataSet, "")
 	s := fmt.Sprintf(format, dataset.DatasetID, dataset.VerticalStart, dataset.VerticalHeight, numPerGroup)
+	scalebox.AppendToFile("/work/messages.txt", "data-grouping-main,"+s)
 
-	scalebox.AppendToFile("/work/messages.txt", "data-grouping-dat,"+s)
+	fmtFitsDataSet := ` {
+		"datasetID":"fits:%s",
+		"keyGroupRegex":"^([0-9]+)/([0-9]+_[0-9]+/[0-9]+)/ch([0-9]{3}).fits$",
+		"keyGroupIndex":"1,3,2",
+		"sinkJob":"message-router-main",
+
+		"groupType":"H",
+		"horizontalWidth":%d
+	}
+	`
+	// Remove space characters
+	format = regexp.MustCompile("\\s+").ReplaceAllString(fmtFitsDataSet, "")
+	s = fmt.Sprintf(format, dataset.DatasetID, dataset.HorizontalWidth)
+	scalebox.AppendToFile("/work/messages.txt", "data-grouping-main,"+s)
 }
