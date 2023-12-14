4cdda9e653b1e0001855c96418b50b5b11a254fd
kaichao <wukaichao@gmail.com>
2023-12-14 23:09:38 +0800

    add dcu-local-mode, and rename untar to unpack.

--- pipelines/main/message-router-go/data_grouping.go
+++ pipelines/main/message-router-go/data_grouping.go
@@ -83,58 +83,69 @@ func fromDataGroupingMain(message string, headers map[string]string) int {
 	//	output: 1257010784/1257010786_1257010815/00001
 
 	if strings.HasSuffix(message, "dat") {
-		ms := strings.Split(message, ",")
-		// first plus last
-		str := ms[0] + "," + ms[len(ms)-1]
-		fmt.Println(str)
-
-		datPattern := "([0-9]+)/[0-9]+_([0-9]+)_ch([0-9]{3}).dat"
-		format := "^%s,%s$"
-		reDat := regexp.MustCompile(fmt.Sprintf(format, datPattern, datPattern))
-		ss := reDat.FindStringSubmatch(str)
-		if ss == nil {
-			fmt.Fprintf(os.Stderr, "[ERROR] Not valid format, message:%s\n", message)
-			return 99
-		}
-		ds := ss[1]
-		start := ss[2]
-		ch := ss[3]
-		end := ss[5]
-		m := fmt.Sprintf("%s/%s_%s/%s/%s", ds, start, end, ch, currentPointings)
-
-		sinkJob := "beam-maker"
-		n, _ := strconv.Atoi(ch)
-		toHost := hosts[(n-109)%numNodesPerGroup]
-		cmdTxt := fmt.Sprintf("scalebox task add --sink-job %s --to-ip %s %s", sinkJob, toHost, m)
-		// scalebox.ExecShellCommand(cmdTxt)
-		code, stdout, stderr := scalebox.ExecShellCommandWithExitCode(cmdTxt, 10)
-		fmt.Printf("stdout for task-add:\n%s\n", stdout)
-		fmt.Fprintf(os.Stderr, "stderr for task-add:\n%s\n", stderr)
-		return code
-
-		// scalebox.AppendToFile("/work/messages.txt", "beam-maker,"+m)
+		return doDat(message, headers)
 	} else if strings.HasSuffix(message, "fits") {
-		str := strings.Split(message, ",")[0]
-		fits1chPattern := "^([0-9]+/[0-9]+_[0-9]+/([0-9]{5}))/ch.+fits$"
-		reFits1ch := regexp.MustCompile(fits1chPattern)
-		ss := reFits1ch.FindStringSubmatch(str)
-		if ss == nil {
-			fmt.Fprintf(os.Stderr, "[ERROR] Not valid format, message:%s\n", message)
-			return 99
-		}
-		m := ss[1]
-		pointing := ss[2]
-		n, _ := strconv.Atoi(pointing)
-		toHost := hosts[(n-1)%numNodesPerGroup]
-		sinkJob := "fits-merger"
-		cmdTxt := fmt.Sprintf("scalebox task add --sink-job %s --to-ip %s %s", sinkJob, toHost, m)
-		code, stdout, stderr := scalebox.ExecShellCommandWithExitCode(cmdTxt, 10)
-		fmt.Printf("stdout for task-add:\n%s\n", stdout)
-		fmt.Fprintf(os.Stderr, "stderr for task-add:\n%s\n", stderr)
-		return code
-		// scalebox.AppendToFile("/work/messages.txt", "fits-merger,"+m)
+		return doFits(message, headers)
 	} else {
 		fmt.Fprintf(os.Stderr, "[ERROR] Not valid format, message:%s\n", message)
 		return 99
 	}
+}
+
+func doDat(message string, headers map[string]string) int {
+	ms := strings.Split(message, ",")
+	// first plus last
+	str := ms[0] + "," + ms[len(ms)-1]
+
+	datPattern := "([0-9]+)/[0-9]+_([0-9]+)_ch([0-9]{3}).dat"
+	format := "^%s,%s$"
+	reDat := regexp.MustCompile(fmt.Sprintf(format, datPattern, datPattern))
+	ss := reDat.FindStringSubmatch(str)
+	if ss == nil {
+		fmt.Fprintf(os.Stderr, "[ERROR] Not valid format, message:%s\n", message)
+		return 99
+	}
+	ds := ss[1]
+	start := ss[2]
+	ch := ss[3]
+	end := ss[5]
+	m := fmt.Sprintf("%s/%s_%s/%s/%s", ds, start, end, ch, currentPointings)
+
+	sinkJob := "beam-maker"
+	if !localMode {
+		scalebox.AppendToFile("/work/messages.txt", sinkJob+","+m)
+		return 0
+	}
+	n, _ := strconv.Atoi(ch)
+	toHost := hosts[(n-109)%numNodesPerGroup]
+	cmdTxt := fmt.Sprintf("scalebox task add --sink-job %s --to-ip %s %s", sinkJob, toHost, m)
+	code, stdout, stderr := scalebox.ExecShellCommandWithExitCode(cmdTxt, 10)
+	fmt.Printf("stdout for task-add:\n%s\n", stdout)
+	fmt.Fprintf(os.Stderr, "stderr for task-add:\n%s\n", stderr)
+	return code
+}
+
+func doFits(message string, headers map[string]string) int {
+	str := strings.Split(message, ",")[0]
+	fits1chPattern := "^([0-9]+/[0-9]+_[0-9]+/([0-9]{5}))/ch.+fits$"
+	reFits1ch := regexp.MustCompile(fits1chPattern)
+	ss := reFits1ch.FindStringSubmatch(str)
+	if ss == nil {
+		fmt.Fprintf(os.Stderr, "[ERROR] Not valid format, message:%s\n", message)
+		return 99
+	}
+	m := ss[1]
+	sinkJob := "fits-merger"
+	if !localMode {
+		scalebox.AppendToFile("/work/messages.txt", sinkJob+","+m)
+		return 0
+	}
+	pointing := ss[2]
+	n, _ := strconv.Atoi(pointing)
+	toHost := hosts[(n-1)%numNodesPerGroup]
+	cmdTxt := fmt.Sprintf("scalebox task add --sink-job %s --to-ip %s %s", sinkJob, toHost, m)
+	code, stdout, stderr := scalebox.ExecShellCommandWithExitCode(cmdTxt, 10)
+	fmt.Printf("stdout for task-add:\n%s\n", stdout)
+	fmt.Fprintf(os.Stderr, "stderr for task-add:\n%s\n", stderr)
+	return code
 }
