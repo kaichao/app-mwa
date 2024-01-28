994a4adafac191c13c377dd2366b2af228a7c43c
kaichao <wukaichao@gmail.com>
2024-01-28 18:18:51 +0800

    add zstd to presto-search in pipeline main.

--- dockerfiles/presto-search/scripts/run.sh
+++ dockerfiles/presto-search/scripts/run.sh
@@ -8,7 +8,6 @@
 # $RFIARGS              arguments for rfifind
 # $SEARCHARGS           arguments for accelsearch_gpu_4
 
-
 # 1. set the input / output / medium file directory
 
 # m="/1257010784/1257010786_1257011025/00024.fits"
@@ -31,69 +30,63 @@ else
     DIR_DEDISP=/data/mwa/dedisp
 fi
 
+# decompress zst file
+cd $DIR_FITS/$(dirname $1) && [ -f "$(basename $1).fits.zst" ] && zstd -d --rm -f $(basename $1).fits.zst
+
 # 2. check if the file exists
 m=$1
-f_dir=${m}
-if [ ! -f "$DIR_FITS/$f_dir" ]; then
-    echo "[ERROR]invalid input message:$f_dir" >&2 && exit 5
-fi
+f_dir=${m}.fits
+# if [ ! -f "$DIR_FITS/$f_dir" ]; then
+#     echo "[ERROR]invalid input message:$f_dir" >&2 && exit 5
+# fi
 # get the filename without extension
 # arr=($(echo $f_dir | tr "/" "\n"))
 # fname=${arr[2]}
-bname=${f_dir%.*}
+# bname=${f_dir%.*}
+
+bname=$1
+
 # 3. run the programs to dedisperse and search
-
-mkdir -p $DIR_DEDISP/$bname
-mkdir -p $DIR_PNG/$bname
-
+echo "DIR_DEDISP:$DIR_DEDISP/$bname"
+echo "DIR_PNG:$DIR_PNG/$bname"
+mkdir -p $DIR_DEDISP/$bname $DIR_PNG/$bname
 code=$?
-if [ $code -ne 0 ]; then 
-    echo "[ERROR]Error in mkdir:$bname" >&2
-    exit $code
-fi
+[[ $code -ne 0 ]] && echo "[ERROR]Error in mkdir:$bname, ret-code:$code" >&2 && exit 11
+
+echo 1111111
 
 cd $DIR_DEDISP/$bname
 rfifind $RFIARGS -o RFIfile $DIR_FITS/$f_dir
 code=$?
-if [ $code -ne 0 ]; then 
-    echo "[ERROR]Error in dedispersion:$f_dir" >&2
-    cd /work
-    rm -r $DIR_DEDISP/$bname
-    exit $code
-fi
+[[ $code -ne 0 ]] && echo "[ERROR]Error in dedispersion:$f_dir, ret-code:$code" >&2 && rm -rf $DIR_DEDISP/$bname && exit 12
+
+echo 2222222
 
 /app/bin/dedisp.py $DIR_FITS/$f_dir RFIfile_rfifind.mask
-
 code=$?
-if [ $code -ne 0 ]; then 
-    echo "[ERROR]Error in dedispersion:$f_dir" >&2
-    cd /work
-    rm -r $DIR_DEDISP/$bname
-    exit $code
-fi
+[[ $code -ne 0 ]] && echo "[ERROR]Error in dedispersion:$f_dir, ret-code:$code" >&2 && rm -rf $DIR_DEDISP/$bname && exit 13
+
+echo 3333333
 
 python3 /code/presto/examplescripts/ACCEL_sift.py > candidates.txt
-if [ $code -ne 0 ]; then 
-    echo "[ERROR]Error in ACCEL_sift:$f_dir" >&2
-    exit $code
-fi
+[[ $code -ne 0 ]] && echo "[ERROR]Error in ACCEL_sift:$f_dir, ret-code:$code" >&2 && rm -rf $DIR_DEDISP/$bname && exit 14
+
+echo 4444444
+
 # 4. parse candidates.txt, fold at each dm
 /app/bin/fold.py $DIR_FITS/$f_dir candidates.txt
 code=$?
-if [ $code -ne 0 ]; then 
-    echo "[ERROR]Error in folding:$f_dir" >&2
-    cd /work
-    rm -r $DIR_DEDISP/$bname
-    exit $code
-fi
+[[ $code -ne 0 ]] && echo "[ERROR]Error in folding:$f_dir, ret-code:$code" >&2 && rm -rf $DIR_DEDISP/$bname && exit 15
+
+echo 5555555
+echo "DIR_DEDISP:$DIR_DEDISP/$bname"
+echo "DIR_PNG:$DIR_PNG/$bname"
 
 # copy the result to target dir
 
-mv *.pfd* $DIR_PNG/$bname
-mv candidates.txt $DIR_PNG/$bname
+mv *.pfd* $DIR_PNG/$bname && mv candidates.txt $DIR_PNG/$bname
+code=$?
 
 # clean up
-code=$?
-cd /work
 rm -r $DIR_DEDISP/$bname
 exit $code
