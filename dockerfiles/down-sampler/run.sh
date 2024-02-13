65b0e5f4b9c3d0b7c7099065ecf34a28d1000a8d
kaichao <wukaichao@gmail.com>
2024-02-13 17:15:22 +0800

    updateschedule_mode: HEAD to slot_on_head, queue_mode to task_dist_mode

--- dockerfiles/down-sampler/run.sh
+++ dockerfiles/down-sampler/run.sh
@@ -46,7 +46,7 @@ code=$?
 [ "$KEEP_SOURCE_FILE" == "no" ] && echo "${DIR_1CH}/${m}" > ${WORK_DIR}/removed-files.txt
 
 # rename file to normalized
-mv ${DIR_1CHX}/${m}_0001.fits ${DIR_1CHX}/${m} && zstd --rm ${DIR_1CHX}/${m}
+mv ${DIR_1CHX}/${m}_0001.fits ${DIR_1CHX}/${m} && zstd --long --rm ${DIR_1CHX}/${m}
 code=$?
 [[ $code -ne 0 ]] && echo "[ERROR] rename fits file and zstd compress " >&2 && exit $code
 
