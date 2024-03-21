49cd62587663709f0c180dbfd60b51da65296578
kaichao <wukaichao@gmail.com>
2024-03-21 18:21:43 +0800

    add batch-insert in createSemaphores().

--- pipelines/main/message-router-go/semaphore.go
+++ pipelines/main/message-router-go/semaphore.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	scalebox "github.com/kaichao/scalebox/golang/misc"
+	"github.com/sirupsen/logrus"
 )
 
 func (cube *DataCube) createDatReadySemaphores() {
@@ -14,6 +15,7 @@ func (cube *DataCube) createDatReadySemaphores() {
 	// 1257010784/1257010786_1257010815/112/00001_00024
 	// 1257010784/1257010786_1257010815/112
 	ts := cube.getTimeRanges()
+	semaArr := []Sema{}
 	for i := 0; i < len(ts); i += 2 {
 		// all dat files in current range
 		initValue := ts[i+1] - ts[i] + 1
@@ -21,14 +23,15 @@ func (cube *DataCube) createDatReadySemaphores() {
 			sema := fmt.Sprintf("dat-ready:%s/t%d_%d/ch%d",
 				cube.DatasetID, ts[i], ts[i+1], ch)
 			fmt.Printf("In createDatReadySemaphores(),sema:%s,init-value:%d\n", sema, initValue)
-			createSemaphore(sema, initValue)
+			semaArr = append(semaArr, Sema{name: sema, value: initValue})
 		}
 	}
+	doInsert(semaArr)
 }
-
 func (cube *DataCube) createPointingBatchLeftSemaphores() {
 	// pointing-batch-left:1257010784/t1257010786_1257010845/ch119
 	initValue := cube.getNumOfPointingBatch()
+	semaArr := []Sema{}
 
 	ts := cube.getTimeRangesWithinInterval(cube.TimeBegin, cube.TimeBegin+cube.NumOfSeconds-1)
 	for i := 0; i < len(ts); i += 2 {
@@ -37,9 +40,10 @@ func (cube *DataCube) createPointingBatchLeftSemaphores() {
 			sema := fmt.Sprintf("pointing-batch-left:%s/t%d_%d/ch%d",
 				cube.DatasetID, ts[i], ts[i+1], ch)
 			fmt.Printf("In createPointingBatchLeftSemaphores(), sema:%s,init-value:%d\n", sema, initValue)
-			createSemaphore(sema, initValue)
+			semaArr = append(semaArr, Sema{name: sema, value: initValue})
 		}
 	}
+	doInsert(semaArr)
 }
 func (cube *DataCube) createFits24chReadySemaphores() {
 	// TARGET: fits-merger
@@ -48,21 +52,24 @@ func (cube *DataCube) createFits24chReadySemaphores() {
 	initValue := 24
 
 	ts := cube.getTimeRanges()
+	semaArr := []Sema{}
 
 	for p := cube.PointingBegin; p <= cube.PointingEnd; p++ {
 		for i := 0; i < len(ts); i += 2 {
 			sema := fmt.Sprintf("fits-24ch-ready:%s/p%05d/t%d_%d",
 				cube.DatasetID, p, ts[i], ts[i+1])
 			fmt.Printf("In createFits24chReadySemaphores(), sema:%s,init-value:%d\n", sema, initValue)
-			createSemaphore(sema, initValue)
+			semaArr = append(semaArr, Sema{name: sema, value: initValue})
 		}
 	}
+	doInsert(semaArr)
 }
 
 func (cube *DataCube) createDatProcessedSemaphores() {
 	// dat-processed:1257010784/p00001_00096/t1257010846_1257010905/ch111
 	// first batch
 	ts := cube.getTimeRanges()
+	semaArr := []Sema{}
 
 	fmt.Printf("cube:%v\n", cube)
 
@@ -75,10 +82,11 @@ func (cube *DataCube) createDatProcessedSemaphores() {
 				sema := fmt.Sprintf("dat-processed:%s/p%05d_%05d/t%d_%d/ch%d",
 					cube.DatasetID, p0, p1, ts[i], ts[i+1], ch)
 				fmt.Printf("In createDatProcessedSemaphores(), sema:%s,init-value:%d\n", sema, p1-p0+1)
-				createSemaphore(sema, p1-p0+1)
+				semaArr = append(semaArr, Sema{name: sema, value: p1 - p0 + 1})
 			}
 		}
 	}
+	doInsert(semaArr)
 }
 
 func (cube *DataCube) getSemaPointingBatchIndex(t int, ch int) int {
@@ -143,4 +151,62 @@ func getSemaphore(semaName string) int {
 	}
 
 	return code
+}
+
+// Sema ...
+type Sema struct {
+	name  string
+	value int
+}
+
+func doInsert(values []Sema) {
+	if !batchInsert {
+		for _, sema := range values {
+			createSemaphore(sema.name, sema.value)
+		}
+		return
+	}
+	// start transaction
+	tx, err := db.Begin()
+	if err != nil {
+		logrus.Errorf("err:%v\n", err)
+	}
+	defer tx.Rollback()
+
+	jobID, _ := strconv.Atoi(os.Getenv("JOB_ID"))
+	sqlText := `
+		INSERT INTO t_semaphore(name,value,value0,job)
+		VALUES($1,$2,$2,$3)
+		ON CONFLICT (name,job) DO UPDATE SET (value,value0) = ($2,$2)
+	`
+
+	batchSize := 100
+	for i := 0; i < len(values); i += batchSize {
+		stmt, err := tx.Prepare(sqlText)
+		if err != nil {
+			logrus.Errorf("err:%v\n", err)
+		}
+		defer stmt.Close()
+
+		end := i + batchSize
+		if end > len(values) {
+			end = len(values)
+		}
+
+		for _, v := range values[i:end] {
+			if _, err := stmt.Exec(v.name, v.value, jobID); err != nil {
+				logrus.Errorf("err:%v\n", err)
+			}
+		}
+		if err = tx.Commit(); err != nil {
+			logrus.Errorf("err:%v\n", err)
+		}
+
+		fmt.Printf("[%d..%d], %d row(s) inserted.\n", i, end, end-i)
+
+		// start next batch
+		if tx, err = db.Begin(); err != nil {
+			logrus.Errorf("err:%v\n", err)
+		}
+	}
 }
