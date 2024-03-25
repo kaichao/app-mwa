b9ee25387d2f516daf65a84610d94f9304e34f51
kaichao <wukaichao@gmail.com>
2024-03-25 14:33:09 +0800

    add down-sampler to pipeline perf-test.

--- pipelines/perf-test/message-router/code/from-fits-merger.sh
+++ pipelines/perf-test/message-router/code/from-fits-merger.sh
@@ -5,4 +5,6 @@ filename="/data/mwa/24ch/$1"
 # remove 24ch-file
 [ "$KEEP_24CH_FILE" = "no" ] && echo "$filename be removed" && rm -f $filename
 
+echo "finished from-fits-merger." > $WORK_DIR/custom-out.txt
+
 exit 0
