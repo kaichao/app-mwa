0ebace6b84a2b4e2bdfeca5fa326710ac9acde22
kaichao <wukaichao@gmail.com>
2024-04-01 10:44:32 +0800

    update for pipeline main refactor,it's working.

--- dockerfiles/local-tar-pull/code/run.sh
+++ dockerfiles/local-tar-pull/code/run.sh
@@ -3,9 +3,17 @@
 # <user>@<ip-addr>/raid0/tmp/mwa/tar1257010784~1257010784/1257010786_1257010815_ch109.dat.tar.zst~/dev/shm/scalebox/mydata/mwa/tar~01
 m0=$1
 
-# remove last 3 characters
+# remove last characters ~b01
 m="${m0%~*}"
+batch="${m0##*~}"
 
 /app/share/bin/run.sh $m
+
+# 消息加上批次号，以免在多批次时有同名冲突
+file="${WORK_DIR}/messages.txt"
+if [[ -f "$file" && $(wc -l < "$file") -eq 1 ]]; then
+    line=$(head -n 1 $file)
+    echo "${line}~${batch}" > "$file"
+fi
 
 exit $?
