f7ec2175b90ba86abbfb139fa6c5482697424510
kaichao <wukaichao@gmail.com>
2024-04-29 15:41:22 +0800

    update pipeline perf-test in p419.

--- pipelines/perf-test/message-router/code/default.sh
+++ pipelines/perf-test/message-router/code/default.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 PB=1
-PE=480
+PE=576
+
+te=1257011085
 
 for ((p = PB; p <= PE; p += 1)); do
-    sema=$(printf "fits-24ch-ready:1257010784/p%05d/t1257010786_1257010815" "$p")
+    sema=$(printf "fits-24ch-ready:1257010784/p%05d/t1257010786_%s" "$p" "$te")
     echo "$sema"
     scalebox semaphore create $sema 24
 done
@@ -12,7 +14,7 @@ done
 for ch in {109..132}; do
     for ((pb = PB; pb <= PE; pb += 24)); do
         pe=$((pb + 23))
-        m=$(printf "1257010784/1257010786_1257010815/%03d/%05d_%05d\n" "$ch" "$pb" "$pe")
+        m=$(printf "1257010784/1257010786_%s/%03d/%05d_%05d\n" "$te" "$ch" "$pb" "$pe")
         echo "beam-maker,$m" >> ${WORK_DIR}/messages.txt
     done
 done
