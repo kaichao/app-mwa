9fb39a2cf954f1a6d32677145a9c9fa90bb265aa
zhzhang <zhzhang2022@cnic.cn>
2024-09-02 13:42:15 +0800

    pipeline presto update

--- pipelines/presto-search/message-router/code/from-dir-list.sh
+++ pipelines/presto-search/message-router/code/from-dir-list.sh
@@ -7,8 +7,8 @@ headers=""
 # code=$?
 # [ $code -ne 0 ] && echo "[ERROR] while creating semaphores! " >&2 && exit $code 
 
-ip_list=("10.11.16.79" "10.11.16.76" "10.11.16.75")
-pattern="^([^~]+)~([0-9]+)/p([0-9]+)"
+# ip_list=()
+pattern="^([^~]+)~([^/]+)/p([0-9]+)"
 if [[ $m =~ $pattern ]]; then
     p=${BASH_REMATCH[3]}
     echo "p: $p"
@@ -23,7 +23,11 @@ echo $m
 
 # echo "rsync-pull,$m" >> ${WORK_DIR}/messages.txt
 pi=$(( 10#$p ))
-echo $pi
-order=$(( ($pi - 1) % ${#ip_list[@]} ))
-echo $order
-scalebox task add --sink-job local-copy-unpack --to-ip ${ip_list[$order]} ${m}
+if [ $pi -lt $POINTING_BEGIN ] || [ $pi -gt $POINTING_END ] ; then
+    echo "$pi out of range: $POINTING_BEGIN to $POINTING_END. omitting." >${WORK_DIR}/custom-out.txt && exit 10
+fi
+order=$(( ($pi - 1) % $NUM_OF_NODES ))
+node=$(printf "%04d.p419" "$order")
+echo $node
+echo $NODES_GROUP
+scalebox task add --sink-job local-copy-unpack --to-host ${NODES_GROUP}-${node} ${m}
