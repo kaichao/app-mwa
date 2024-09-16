22bf6f56cffbd37714d67e9fbf1da8171c52a77a
kaichao <wukaichao@gmail.com>
2024-09-16 18:48:53 +0800

    Change variables to arguments in pipeline main, and update datacube def.

--- pipelines/main/message-router-go/datacube/datacube.go
+++ pipelines/main/message-router-go/datacube/datacube.go
@@ -1,37 +1,12 @@
 package datacube
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
 
-	"github.com/kaichao/scalebox/pkg/misc"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 )
-
-type dataCube struct {
-	DatasetID string
-
-	ChannelBegin  string
-	NumOfChannels string
-
-	TimeBegin    string
-	NumOfSeconds string
-	// 40
-	TimeUnit string
-	// 40的倍数
-	TimeStep string
-
-	PointingBegin string
-	PointingEnd   string
-	// 通常取24
-	PointingStep string
-	NumPerBatch  string
-}
 
 // DataCube ...
 //
@@ -60,46 +35,6 @@ type DataCube struct {
 	NumPerBatch int `yaml:"numPerBatch"`
 }
 
-func getDataCubeFromDB(datasetID string) *DataCube {
-	cmdText := "scalebox dataset get-metadata " + datasetID
-	code, stdout, stderr := misc.ExecShellCommandWithExitCode(cmdText, 10)
-	fmt.Fprintf(os.Stderr, "stderr for dataset-get-metadata:\n%s\n", stderr)
-	if code != 0 {
-		fmt.Fprintf(os.Stderr, "[WARN] error for dataset-get-metadata dataset=%s in getDataCube()\n", datasetID)
-		return nil
-	}
-
-	var (
-		dc       dataCube
-		datacube DataCube
-	)
-	if err := json.Unmarshal([]byte(stdout), &dc); err != nil {
-		// skip non-json format error
-		if !strings.HasPrefix(err.Error(), "invalid character") {
-			fmt.Printf("error parsing, err-info:%v\n", err)
-		}
-		// non-datacube definition
-		return nil
-	}
-
-	datacube.DatasetID = dc.DatasetID
-
-	datacube.ChannelBegin, _ = strconv.Atoi(dc.ChannelBegin)
-	datacube.NumOfChannels, _ = strconv.Atoi(dc.NumOfChannels)
-
-	datacube.TimeBegin, _ = strconv.Atoi(dc.TimeBegin)
-	datacube.NumOfSeconds, _ = strconv.Atoi(dc.NumOfSeconds)
-	datacube.TimeUnit, _ = strconv.Atoi(dc.TimeUnit)
-	datacube.TimeStep, _ = strconv.Atoi(dc.TimeStep)
-
-	datacube.PointingBegin, _ = strconv.Atoi(dc.PointingBegin)
-	datacube.PointingEnd, _ = strconv.Atoi(dc.PointingEnd)
-	datacube.PointingStep, _ = strconv.Atoi(dc.PointingStep)
-	datacube.NumPerBatch, _ = strconv.Atoi(dc.NumPerBatch)
-
-	return &datacube
-}
-
 var (
 	datacubeFile = "/dataset.yaml"
 	// datacubeFile = "/dataset-perf-test.yaml"
@@ -110,7 +45,7 @@ var (
 )
 
 func getDataCubeFromFile(datasetID string) *DataCube {
-	config := map[string]map[string]map[string]DataCube{}
+	config := map[string]map[string]DataCube{}
 
 	yamlFile, err := ioutil.ReadFile(datacubeFile)
 	if err != nil {
@@ -122,10 +57,10 @@ func getDataCubeFromFile(datasetID string) *DataCube {
 	}
 
 	fmt.Println("config:", config)
-	cube := config["datasets"][datasetID]["metadata"]
+	cube := config["datasets"][datasetID]
 	if cube.NumOfSeconds == 0 {
 		cube.NumOfSeconds = cube.TimeEnd - cube.TimeBegin + 1
 	}
-	// fmt.Println(cube)
+
 	return &cube
 }
