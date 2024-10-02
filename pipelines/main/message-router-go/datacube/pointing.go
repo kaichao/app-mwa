7ac2f7fbf7c812d8327fc1ccd4bb0bcc0a294742
kaichao <wukaichao@gmail.com>
2024-10-02 20:40:41 +0800

    Refactor message-rouoter-go for multi-block compute nodes.

--- pipelines/main/message-router-go/datacube/pointing.go
+++ pipelines/main/message-router-go/datacube/pointing.go
@@ -17,7 +17,7 @@ func (cube *DataCube) GetNumOfPointingBatch() int {
 	return result
 }
 
-// 获得全部指向的指向区间
+// 获得全部指向的指向区间(not used)
 func (cube *DataCube) getPointingRanges() []int {
 	var ret []int
 	for p0 := cube.PointingBegin; p0 <= cube.PointingEnd; p0 += cube.PointingStep {
@@ -31,6 +31,7 @@ func (cube *DataCube) getPointingRanges() []int {
 	return ret
 }
 
+// GetPointingRangesByBatchIndex ...
 func (cube *DataCube) GetPointingRangesByBatchIndex(batchIndex int) []int {
 	numBatch := (cube.PointingEnd - cube.PointingBegin + 1) / (cube.NumPerBatch * cube.PointingStep)
 	if batchIndex < 0 || batchIndex > numBatch {
@@ -59,6 +60,7 @@ func (cube *DataCube) getPointingRangesByBatch(batchBegin, batchEnd int) []int {
 	return ret
 }
 
+// GetPointingBatchIndex ...
 // 获取当前指向所在的批次索引
 func (cube *DataCube) GetPointingBatchIndex(p int) int {
 	if cube.PointingBegin > p || p > cube.PointingEnd {
@@ -67,6 +69,7 @@ func (cube *DataCube) GetPointingBatchIndex(p int) int {
 	return (p - cube.PointingBegin) / (cube.PointingStep * cube.NumPerBatch)
 }
 
+// GetPointingBatchRange ...
 // 获得当前指向所在的批次指向区间
 func (cube *DataCube) GetPointingBatchRange(p int) (int, int) {
 	index := cube.GetPointingBatchIndex(p)
