74f6b411ab54a742014ac7fc5e04016ffaba4666
tangjianings <tangjianing@cnic.cn>
2024-10-12 14:17:02 +0800

    mwa-prep Local transmission version

--- dockerfiles/untar/README.md
+++
@@ -1,4 +0,0 @@
-# 环境变量
-
- SOURCE_URL=输入地址
- OUTPUT_URL=输出地址
--- dockerfiles/untar/run.sh
+++ dockerfiles/untar/run.sh
@@ -1,44 +1,64 @@
 #!/bin/bash
 #传参：tar文件路径
-result=$(echo "$1" | awk -F "%" '{print $2}')
-echo $result
+#result=$(echo "$1" | awk -F "~" '{print $2}')
+result="${1#*~}"
+#tar1266932744/1266932744_1266935421x.tar.zst
+
+echo "result:"$result
 input_file="/local"${SOURCE_URL}/$result
 InputBytes=$(stat --printf="%s" ${input_file})
+exit_code=$?
 echo $InputBytes
 #获取文件名
 in_file=$(basename ${input_file})
 cd /work
 #复制文件到当前目录
 cp ${input_file} .
+exit_code=$?
 #path=$(dirname "$1")
 #work_1是暂存解压后的dat文件，在zst压缩过程会删除
 #如果文件夹不存在，则创建文件夹
 if [ ! -d "work_1" ]; then
 mkdir work_1
 fi
-tar -xf ${in_file} -C /work/work_1
+zstd -d -c ${in_file} | tar -xvf - --transform 's|.*/||' -C /work/work_1
+#tar -xvf ${in_file} --transform 's|.*/||' -C /work/work_1
+
+#tar -xf ${in_file} -C /work/work_1
 #tar -xvf $1 -C $path/work_1
 exit_code=$?
-output_dir="/local"$OUTPUT_URL     # 指定输出目录
+if [ $exit_code -ne 0 ]; then
+    exit ${exit_code}
+fi
+output_dir="/local/data/mwa"     # 指定输出目录
+dat_output_dir="/local"${OUTPUT_URL}
 if [ ! -d "$output_dir" ]; then
   # 目录不存在，使用mkdir命令创建多级目录
   mkdir -p "$output_dir"
 else
   echo "多级目录已存在"
 fi
+
 rm -f ${in_file}
+
 cd work_1
 for myfile in ./*
 do
 filename=$(basename "$myfile")
-zstd --rm "$myfile" -o "${output_dir}/${filename}.zst"
-exit_code=$?
-groupfilename="zst,${output_dir}/${filename}.zst"
-#传给分组
-echo "data-grouping-fits,${groupfilename}" >> /work/messages.txt
-#python3 /app/bin/run.py $groupfilename
-exit_code=$?
+
+#mv ${filename} ${output_dir}
+#send-message $filename
+if [[ $filename == *"ch"* ]]; then
+    mv ${filename} ${output_dir}
+    send-message ${filename}
+else
+    mv ${filename} ${dat_output_dir}
+fi
+
+#zstd --rm "$myfile" -o "${output_dir}/${filename}.zst"
+#exit_code=$?
 done
+#send-message $1
 echo '{
      "inputBytes":'${InputBytes}'
 }' > /work/task-exec.json
