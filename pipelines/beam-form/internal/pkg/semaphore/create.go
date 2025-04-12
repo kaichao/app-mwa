790121877848ca433e41cc1c29b18ca98a723521
kaichao <wukaichao@gmail.com>
2025-04-12 20:12:02 +0800

    Update.

---
+++ pipelines/beam-form/internal/pkg/semaphore/create.go
@@ -0,0 +1,21 @@
+package semaphore
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kaichao/gopkg/common"
+	"github.com/kaichao/gopkg/exec"
+)
+
+// Create ...
+func Create(semaLines string) error {
+	common.AppendToFile("my-sema.txt", semaLines)
+	defer os.Remove("my-sema.txt")
+	cmd := "scalebox semaphore create --sema-file my-sema.txt"
+	code, err := exec.RunReturnExitCode(cmd, 600)
+	if code > 0 {
+		return fmt.Errorf("[ERROR]semaphore-create,code=%d", code)
+	}
+	return err
+}
